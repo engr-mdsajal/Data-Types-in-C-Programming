dfb59f9a09db81201fe750f08ba700b9aa93f299
ifthekar <sajalcse2018@gmail.com>
2023-05-22 16:06:59 +0600

    char data & long double type data program run

---
+++ charDataType.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+   //char data type
+   char ch_value;
+
+   printf("Enter Your Character Value= ");
+   scanf("%c",&ch_value);
+   printf("Character Value is= %c\n",ch_value);
+   printf("Size of Character Value= %d\n",sizeof(ch_value));
+
+
+return 0;
+}
---
+++ long_double.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main(){
+
+   long double long_double_value=3.14e+2;
+
+ //  printf("Enter Your Long Double Value= ");
+ //  scanf("%Lf",&long_double_value);
+   printf("Long Double Value is= %Lf\n",long_double_value);
+   printf("Size of long double Value= %d\n",sizeof(long_double_value));
+
+return 0;
+}
